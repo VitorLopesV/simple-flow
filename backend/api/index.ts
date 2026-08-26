fe06961d7f4519e97cff69fea1eac9d759b9e244
vitor <vitorvtlopes@gmail.com>
2026-08-26 18:12:31 -0300

    Colocar entry point de volta em api/index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,5 +1,5 @@
 import serverlessHttp from 'serverless-http';
-import { createApp } from './app';
+import { createApp } from '../src/app';
 
 const app = createApp();
 export default serverlessHttp(app);
